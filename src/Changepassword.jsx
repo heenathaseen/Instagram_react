e208c71db6fca011936ecf497ee422b2d7f8fb03
heenathaseen <heenathaseen98@gmail.com>
2020-01-25 15:39:38 +0530

    changepwd-form

--- src/Changepassword.jsx
+++ src/Changepassword.jsx
@@ -2,38 +2,68 @@ import React from 'react';
 import './Changepassword.scss';
 import ananya from './ananya.png';
 
-const Changepassword = () => (
-  <div className="edit">
+class Changepassword extends React.Component {
+  state = {
+    oldpassword: '',
+    newpassword: '',
+    confirmpassword: '',
+  };
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handleSubmit = e => {
+      
+    e.preventDefault();
+    console.log(this.state);
+  };
+  render() {
+    const {oldpassword,newpassword,confirmpassword}=this.setState;
+    return (
+      <div className="edit">
 
-    <div className="edit-grid">
-      <div className="left">
-        <img src={ananya} alt="" />
-        <h5>Old Password</h5>
-        <h5 id="one">New Password</h5>
-        <h5 id="2">
-Confirm New
-          {' '}
-          <br />
-          {' '}
-Password
-        </h5>
+        <div className="edit-grid">
+          <div className="left">
+            <img src={ananya} alt="" />
+            <h5>Old Password</h5>
+            <h5 id="one">New Password</h5>
+            <h5 id="2">
+    Confirm New
+              {' '}
+              <br />
+              {' '}
+    Password
+            </h5>
 
 
+          </div>
+          <div className="right">
+            <h3>Ananya Pande</h3>
+
+            <input type="text" placeholder=""
+             name="oldpassword"
+             
+             value={oldpassword}
+             onChange={this.handleChange} 
+             />
+            <input type="text" placeholder="" 
+             name="newpassword"
+             value={newpassword}
+             onChange={this.handleChange}
+            />
+            <input type="text" placeholder="" 
+             name="confirmpassword"
+             value={confirmpassword}
+             onChange={this.handleChange}/>
+            <br />
+            <button type="button" onClick={this.handleSubmit}>Change Password</button>
+            <br />
+            <p>Forgot Password?</p>
+
+
+          </div>
+        </div>
       </div>
-      <div className="right">
-        <h3>Ananya Pande</h3>
-
-        <input type="text" placeholder="" />
-        <input type="text" placeholder="" />
-        <input type="text" placeholder="" />
-         <br></br>
-        <button type="button">Change Password</button>
-        <br></br>
-        <p>Forgot Password?</p>
-
-
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 export default Changepassword;
